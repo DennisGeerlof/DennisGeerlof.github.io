ba350c91807de955fc2725cd1cc3b0c0c8852952
DennisGeerlof <6021889@mborijnland.nl>
2023-09-14 00:06:55 +0200

    commit

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <title>Header</title>
 </head>
 <header>
-    <a href="index.php" id="logo"><img src="images/Logo_Fill.png" height="150px" alt="Logo_Fill"></a>
+    <a href="index.html" id="logo"><img src="images/Logo_Fill.png" height="150px" alt="Logo_Fill"></a>
     <a href="contact.php" class="links">Contact</a>
     <a href="about.php" class="nav, links">About</a>
     <a href="experience.php" class="nav, links">Experience</a>
