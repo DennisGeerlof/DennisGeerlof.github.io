2b2bfe3ccc05423618746f5b9a50c8bd544f830c
DennisGeerlof <6021889@mborijnland.nl>
2024-04-18 18:46:36 +0200

    spot

--- DG-Search/script.js
+++ DG-Search/script.js
@@ -12,9 +12,27 @@ updateTime(); // Update time when the page loads
 // Update time every second
 setInterval(updateTime, 1000);
 
+// Darkmode
 document.getElementById('darkModeToggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
 });
 
+//Spotify pop-up
+document.getElementById("popupButton").addEventListener("click", function() {
+    var popupDiv = document.getElementById("popupDiv");
+    if (popupDiv.style.display === "none" || popupDiv.style.display === "") {
+      popupDiv.style.display = "block";
+    } else {
+      popupDiv.style.display = "none";
+    }
+  });
+  
+  window.onload = function() {
+    // Get all links and buttons in the document
+    var links = document.querySelectorAll("a, button, form");
 
-
+    // Loop through each link/button and set target attribute to "_blank"
+    for (var i = 0; i < links.length; i++) {
+      links[i].setAttribute("target", "_blank");
+    }
+  };
